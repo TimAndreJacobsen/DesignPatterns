af72adb73f6d8b9c1f1afd462cfdf82afa600c76
TimAndreJacobsen <timandrejacobsen@gmail.com>
2021-10-25 20:34:29 +0200

    1 example and one borked

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //// Concrete Builders
+            //var coffeePurchaseOrderBuilder = new CoffeePurchaseOrderBuilder();
+            
+            //// Director
+            //var POP
+
+
+             //Build vehicles and generate report
+
+            var items = new List<Vehicle>
+            {
+                new Vehicle("Honda", "Accord", 479000.00, "Diesel"),
+                new Vehicle("Mazda", "323", 180000.00, "Gasoline"),
+                new Vehicle("Tesla", "Model 3", 600000.00, "Electric"),
+            };
+
+            var VehicleBuilder = new DailyReportBuilder(items);
+            var director = new VehicleBuildDirector(VehicleBuilder);
+
+            director.BuildCompleteReport();
+
+            var directorReport = VehicleBuilder.GetDailyReport();
+
+            Console.WriteLine(directorReport.Debug());
+        }
+    }
+}
---
+++ BuilderPattern/Vehicle.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder_Pattern
+{
+    public class Vehicle
+    {
+        public string Make;
+        public string Model;
+        public double Price;
+        public string FuelType;
+
+        public Vehicle(string make, string model, double price, string FuelType)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Price = price;
+            this.FuelType = FuelType;
+        }
+    }
+
+    public class VehicleSalesReport
+    {
+        public string Title;
+        public string VehicleDetails;
+        public string ReportDetails;
+
+        public string Debug()
+        {
+            return new StringBuilder()
+                .AppendLine(Title)
+                .AppendLine(VehicleDetails)
+                .AppendLine(ReportDetails)
+                .ToString();
+        }
+    }
+
+    public interface IVehicleBuilder
+    {
+        void AddTitle();
+        void AddVehicleDetails();
+        //void AddVehicleStatistics();
+        void AddReportDetails(DateTime dateTime);
+        VehicleSalesReport GetDailyReport();
+    }
+
+    public class DailyReportBuilder : IVehicleBuilder
+    {
+        private VehicleSalesReport _report;
+        private IEnumerable<Vehicle> _items;
+
+        public DailyReportBuilder(IEnumerable<Vehicle> items)
+        {
+            Reset();
+            _items = items;
+        }
+
+        public void AddTitle()
+        {
+            _report.Title = "------- Daily Sales Report ------- \n\n";
+        }
+
+        public void AddVehicleDetails()
+        {
+            _report.VehicleDetails = string.Join(Environment.NewLine, _items.Select(vehicle =>
+            $"Vehicle: {vehicle.Make} - {vehicle.Model} \n" +
+            $"Powered by {vehicle.FuelType}! \n" +
+            $"Price: {vehicle.Price} \n"));
+        }
+
+        public void AddReportDetails(DateTime dateTime)
+        {
+            _report.ReportDetails = $"Report generated on {dateTime}";
+        }
+
+        public VehicleSalesReport GetDailyReport()
+        {
+            VehicleSalesReport finishedReport = _report;
+            Reset();
+
+            return finishedReport;
+        }
+
+        public void Reset()
+        {
+            _report = new VehicleSalesReport();
+        }
+    }
+
+
+    public class VehicleBuildDirector
+    {
+        private IVehicleBuilder _builder;
+
+        public VehicleBuildDirector(IVehicleBuilder concreteBuilder)
+        {
+            _builder = concreteBuilder;
+        }
+
+        public void BuildCompleteReport()
+        {
+            _builder.AddTitle();
+            _builder.AddVehicleDetails();
+            _builder.AddReportDetails(DateTime.Now);
+        }
+    }
+}
